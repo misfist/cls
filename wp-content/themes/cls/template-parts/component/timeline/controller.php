4f0da6059c3c6dd9ab8a49740c0d7ca8fe4e6c89
Pea <pea@misfist.com>
2019-10-07 14:53:55 -0400

    Add wrapper around timeline controls

--- wp-content/themes/cls/template-parts/component/timeline/controller.php
+++ wp-content/themes/cls/template-parts/component/timeline/controller.php
@@ -9,19 +9,26 @@
 ?>
 
 <nav id="timeline-controller-module" class="timeline-controls"  role="pagination">
-	<a href="#" class="previous ">&lt;<span class="screen-reader-text"><?php esc_html_e( 'Previous', 'cls' ); ?></span></a>
-	
-	<ul id="timeline-controls" class="timeline-nav" data-rows="<?php echo $rows; ?>" style="grid-template-rows: repeat(<?php echo $rows; ?>, auto);" data-tabs>
-	<?php
-	$index = 0;
-	foreach( $terms as $term ) : ?>
+	<div class="timeline-wrapper wrapper">
+		<div class="timeline-pagination">
+			<a href="#" class="previous ">&lt;<span class="screen-reader-text"><?php esc_html_e( 'Previous', 'cls' ); ?></span></a>
+			<a href="#" class="next">&gt;<span class="screen-reader-text"><?php esc_html_e( 'Next', 'cls' ); ?></span></a>
+		</div>
+		
+		<ul id="timeline-controls" class="timeline-nav" data-rows="<?php echo $rows; ?>" style="grid-template-rows: repeat(<?php echo $rows; ?>, auto);" data-tabs>
+		<?php
+		$index = 0;
+		foreach( $terms as $term ) : ?>
 
-		<li id="<?php echo $term->term_id; ?>" class="timeline-tab-title<?php echo ( 0 === $index ) ? ' is-active' : '' ;?>"><a href="#year-<?php echo $term->slug;?>"><?php echo $term->name;?></a></li>
+			<li id="<?php echo $term->term_id; ?>" class="timeline-tab-title<?php echo ( 0 === $index ) ? ' is-active' : '' ;?>"><a href="#year-<?php echo $term->slug;?>"><?php echo $term->name;?></a></li>
 
-	<?php
-	$index++;
-	endforeach; ?>
-	</ul>
-	<a href="#" class="next">&gt;<span class="screen-reader-text"><?php esc_html_e( 'Next', 'cls' ); ?></span></a>
+		<?php
+		$index++;
+		endforeach; ?>
+		</ul>
+
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div><!-- .timeline-wrapper -->
 </nav><!-- .timeline-controls -->
-<div id="timeline-modal" class="reveal" role="dialog" data-reveal></div>
