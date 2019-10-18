a9722ee7e4a1ca298b31f974323601f98233916a
Pea <pea@misfist.com>
2019-10-18 19:13:14 -0400

    Updated counter component

--- wp-content/themes/cls/blocks/number-counter/block.php
+++ wp-content/themes/cls/blocks/number-counter/block.php
@@ -3,4 +3,18 @@
  * Block: Number Counter
  */
 ?>
-<?php get_template_part( './blocks/number-counter/view' ); ?>
+<?php if ( block_rows( 'counter' ) ): ?>
+    <?php $columns = count( block_value( 'counter' )['rows'] ); ?>
+    <div class="wp-block-number-counter has-<?php echo $columns; ?>-columns">
+        <div class="number-counter__inner-container inner-container">
+        <?php while( block_rows( 'counter' ) ) : 
+            block_row( 'counter' ); ?>
+
+            <?php get_template_part( './blocks/number-counter/view' ); ?>
+        
+        <?php endwhile; ?>
+        </div>
+    </div>
+<?php endif; 
+reset_block_rows( 'counter' );
+?>
--- wp-content/themes/cls/blocks/number-counter/view.php
+++ wp-content/themes/cls/blocks/number-counter/view.php
@@ -3,14 +3,10 @@
  * Block: Home Callout
  */
 ?>
-<div class="wp-block-number-counter">
-    <div class="number-counter__inner-container inner-container">
-        <?php
-        $number = block_value( 'number' );
-        ?>
-        <header class="block-heading"><?php block_field( 'heading' ); ?></header>
-        <div class="block-number" data-number="<?php echo number_format( $number, 0, '', '' ); ?>"><?php echo number_format( $number ); ?></div>
-        <figure class="block-image"><img src="<?php block_field( 'image' ); ?>" /></figure>
-        <div class="block-content"><?php block_field( 'content' ); ?></div>
-    </div>
+<div class="number-counter">
+    <?php $number = block_sub_value( 'number' ); ?>
+    <header class="block-heading"><?php block_sub_field( 'heading' ); ?></header>
+    <div class="block-number" data-number="<?php echo number_format( $number, 0, '', '' ); ?>"><?php echo number_format( $number ); ?></div>
+    <figure class="block-image"><img src="<?php block_sub_field( 'image' ); ?>" /></figure>
+    <div class="block-content"><?php block_sub_field( 'content' ); ?></div>
 </div>
