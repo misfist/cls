1fdafe29308b2ea5c2ae71fb564f987009801551
Pea <pea@misfist.com>
2019-10-23 00:37:36 -0400

    Add rest support to event catgory

--- wp-content/plugins/site-functionality/includes/filters.php
+++ wp-content/plugins/site-functionality/includes/filters.php
@@ -27,9 +27,11 @@ add_filter( 'page-links-to-post-types', 'site_functions_page_links_to_post_types
 
 /**
  * Add REST API support to an already registered post type.
+ * 
+ * @param array $args
+ * @param string $post_type
+ * @return array $args
  */
-// add_filter( 'register_post_type_args', 'site_functions_post_type_args', 10, 2 );
- 
 function site_functions_post_type_args( $args, $post_type ) {
  
     if ( 'event' === $post_type ) {
@@ -38,3 +40,17 @@ function site_functions_post_type_args( $args, $post_type ) {
  
     return $args;
 }
+add_filter( 'register_post_type_args', 'site_functions_post_type_args', 10, 2 );
+
+/**
+ * Add REST API support to an already registered taxonomy
+ *
+ * @return void
+ */
+function site_functions_modify_taxonomy() {
+    $args = get_taxonomy( 'event' );
+    $args->show_in_rest = true;
+
+    register_taxonomy( 'event-category', 'event', (array) $args );
+}
+add_action( 'init', 'site_functions_modify_taxonomy', 11 );
