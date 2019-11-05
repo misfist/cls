b02987af0875715d2ddf6357c76f030d2d4b4cd3
Pea <pea@misfist.com>
2019-11-05 12:19:28 -0500

    23 - Current Clinics spacing
    Modified markup to prohibit addition of empty p tags!

--- wp-content/themes/cls/blocks/adhoc-posts/view.php
+++ wp-content/themes/cls/blocks/adhoc-posts/view.php
@@ -21,16 +21,15 @@
                     <div class="entry-body">
                         <header class="entry-header">
                             <h3 class="entry-title"><a href="<?php echo esc_url( block_sub_value( 'link' ) ); ?>"<?php echo ( block_sub_value( 'target' ) ) ? ' target="_blank"' : ''; ?>><?php block_sub_field( 'title' ); ?></a></h3>
-                        </header><!-- .entry-header -->
+                        </header>
                         <div class="entry-content">
                             <?php if( $excerpt = block_sub_value( 'excerpt' ) ) : ?>
                                 <?php echo apply_filters( 'the_content', $excerpt ); ?>
                             <?php endif; ?>
-                        </div><!-- .entry-content -->
+                        </div>
                         <footer class="entry-footer">
                             <a href="<?php echo esc_url( block_sub_value( 'link' ) ); ?>" class="read-more"<?php echo ( block_value('target') ) ? ' target="_blank"' : ''; ?>><?php esc_html_e( 'Read More', 'cls' ); ?></a>
-                        </footer><!-- .entry-footer -->                    
-                    </div>
+                        </footer></div>
                 </article><!-- .post -->
             <?php
             endwhile;
