8847cf316c92d31d35b1b33b29b78ccd8bbb90bd
Pea <pea@misfist.com>
2019-11-06 19:44:41 -0500

    79 - Remove post navigation

--- wp-content/themes/cls/single.php
+++ wp-content/themes/cls/single.php
@@ -16,16 +16,6 @@ get_header(); ?>
 
 		get_template_part( 'template-parts/content/content', get_post_type() );
 
-		the_post_navigation( array(
-			'prev_text' => '&larr; %title',
-			'next_text' => '%title &rarr;',
-		) );
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) :
-			comments_template();
-		endif;
-
 	endwhile; // End of the loop.
 	?>
 
