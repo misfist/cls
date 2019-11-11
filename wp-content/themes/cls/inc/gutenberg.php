402895dc503807a3df9cee1409a119b710f5c662
Pea <pea@misfist.com>
2019-11-10 19:58:20 -0500

    Disable gutenberg for specific templates

--- wp-content/themes/cls/inc/gutenberg.php
+++ wp-content/themes/cls/inc/gutenberg.php
@@ -5,4 +5,43 @@
  * @package cls
  */
 
- 
+ /**
+ * Templates & pages without Gutenberg editor
+ *
+ */
+function cls_exclude_from_gutenberg( $id = false ) {
+
+	$excluded_templates = array(
+		'page-templates/front-page.php',
+	);
+
+	$excluded_ids = array(
+		// get_option( 'page_on_front' )
+	);
+
+	if( empty( $id ) )
+		return false;
+
+	$id = intval( $id );
+	$template = get_page_template_slug( $id );
+
+	return in_array( $id, $excluded_ids ) || in_array( $template, $excluded_templates );
+}
+
+/**
+ * Disable Gutenberg by template
+ *
+ */
+function cls_disable_gutenberg( $can_edit, $post_type ) {
+
+	if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+		return $can_edit;
+
+	if( cls_exclude_from_gutenberg( $_GET['post'] ) )
+		$can_edit = false;
+
+	return $can_edit;
+
+}
+add_filter( 'gutenberg_can_edit_post_type', 'cls_disable_gutenberg', 10, 2 );
+add_filter( 'use_block_editor_for_post_type', 'cls_disable_gutenberg', 10, 2 );
