f637d656ba9b03ec6bdaa65d1ba519fae41752ae
Pea <pea@misfist.com>
2019-12-12 15:07:24 -0500

    Add Link title

--- wp-content/themes/cls/template-parts/content/page-footer/back-link-page.php
+++ wp-content/themes/cls/template-parts/content/page-footer/back-link-page.php
@@ -11,7 +11,7 @@ if( !function_exists( 'get_field' ) )  return; ?>
 <?php if( $link = get_field( 'back_to_link' ) ) : ?>
     <div class="entry-footer">
         <div class="wp-block-custom-back-link">
-            <a href="<?php echo esc_url( $link['url'] ); ?>"><?php esc_html_e( 'Back to Page', 'cls' ); ?></a>
+            <a href="<?php echo esc_url( $link['url'] ); ?>"><?php esc_html_e(  $link['title'], 'cls' ); ?></a>
         </div>
     </div><!-- .entry-footer -->
 <?php endif; ?>
