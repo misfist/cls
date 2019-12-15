59d3ae16bee9ec593f1a0d6079b54da95c3bbce0
Pea <pea@misfist.com>
2019-12-15 12:40:29 -0500

    News announcement styling

--- wp-content/themes/cls/template-parts/header/site-notice.php
+++ wp-content/themes/cls/template-parts/header/site-notice.php
@@ -7,6 +7,6 @@
 <div class="site-notice" data-closable>
     <?php echo apply_filters( 'the_content', get_theme_mod( 'notice_text' ) ); ?>
     <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-        <span aria-hidden="true">&times;</span>
+        <span aria-hidden="true" class="icon close"></span>
     </button>
 </div>
