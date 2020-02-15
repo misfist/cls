850531b9e75515ca60e026ac7e33cdbad597b3b8
Pea <pea@misfist.com>
2020-02-15 16:09:20 -0500

    Add copyCustomJS task to config

--- wp-content/themes/cls/gulpfile.babel.js
+++ wp-content/themes/cls/gulpfile.babel.js
@@ -258,7 +258,7 @@ function watch() {
 
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
-  gulp.series(clean, gulp.parallel(sass, 'webpack:build', images, copy)));
+  gulp.series(clean, gulp.parallel(sass, 'webpack:build', images, copy, copyCustomJS)));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
