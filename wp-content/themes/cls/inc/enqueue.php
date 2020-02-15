e80aabdf94773c6c84625984ef3670ceb4871002
Pea <pea@misfist.com>
2020-02-15 11:49:32 -0500

    Add mobile image to home page

--- wp-content/themes/cls/inc/enqueue.php
+++ wp-content/themes/cls/inc/enqueue.php
@@ -16,30 +16,23 @@ function cls_theme_fonts() {
 	 * supported by Raleway, translate this to 'off'. Do not translate
 	 * into your own language.
 	 */
-	$raleway = esc_html_x( 'on', 'Raleway font: on or off', 'cls' );
+	$font_families[] = 'Raleway:400,600,700,800,900';
 
-	if ( 'off' !== $raleway ) {
-		$font_families = array();
-		$font_families[] = 'Raleway:400,600,700,800,900';
-		// $font_families[] = 'Raleway:900';
+	$query_args = array(
+		'family'  => urlencode( implode( '|', $font_families ) ),
+		'subset'  => urlencode( 'latin,latin-ext' ),
+		'display' => 'swap'
+	);
 
-		$query_args = array(
-			'family'  => urlencode( implode( '|', $font_families ) ),
-			'subset'  => urlencode( 'latin,latin-ext' ),
-			'display' => 'swap'
-		);
-
-		$fonts_url = add_query_arg( $query_args, esc_url( 'https://fonts.googleapis.com/css' ) );
-	}
+	$fonts_url = add_query_arg( $query_args, esc_url( 'https://fonts.googleapis.com/css' ) );
 
 	return $fonts_url;
-
 }
 
  /**
  * Enqueue scripts and styles.
  */
-function gutenberg_starter_theme_scripts() {
+function cls_enqueue_scripts() {
 
 	/** Don't load Custom Twitter Feeds styling */
 	wp_dequeue_style( 'ctf_styles' );
@@ -52,13 +45,32 @@ function gutenberg_starter_theme_scripts() {
 		wp_enqueue_style( 'cls-style', get_template_directory_uri() . '/dist/assets/css/style.css' );
 	}
 
+	/** Mobile image on home page */
+	/*
+		medium: 934px
+		large: 1200px
+		xlarge: 1320px
+	*/
+	$mobile_image = cls_get_home_mobile_img();
+	$mobile_hero_css = "
+		@media screen and ( max-width: 934px ) {
+			.wp-block-cover.hero {
+				background-image: url('$mobile_image') !important;
+			}
+		}
+		";
+	
+	if( is_front_page() ) {
+		wp_add_inline_style( 'cls-style', $mobile_hero_css );
+	}
+
 	wp_enqueue_style( 'cls-print', get_template_directory_uri() . '/dist/assets/css/print.css' );
 
 	wp_enqueue_style( 'cls-fonts', cls_theme_fonts(), null, null );
 
 	wp_enqueue_script( 'cls-scripts', get_template_directory_uri() . '/dist/assets/js/app.js', array( 'jquery' ), '20151215', true );
 }
-add_action( 'wp_enqueue_scripts', 'gutenberg_starter_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'cls_enqueue_scripts' );
 
 /**
  * Function to turn off inline tabs block css.
@@ -76,3 +88,18 @@ function cls_unregister_tabs_inline_css( $enable, $type, $uniqueid ) {
 	return $enable;
 }
 add_filter( 'kadence_blocks_render_inline_css', 'cls_unregister_tabs_inline_css', 10, 3 );
+
+/**
+ * Get Mobile Image used on home page
+ *
+ * @return mixed image || ''
+ */
+function cls_get_home_mobile_img() {
+	$page_id = get_option( 'page_on_front' );
+	if( $image_id = get_post_meta( $page_id, 'sections_0_mobile_image', true ) ) {
+		if( $image_src_array = wp_get_attachment_image_src( $image_id, 'full' ) ) {
+			return esc_url( $image_src_array[0] );
+		}
+	}
+	return '';
+}
--- wp-content/themes/cls/inc/template-functions.php
+++ wp-content/themes/cls/inc/template-functions.php
@@ -11,7 +11,7 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function gutenberg_starter_theme_body_classes( $classes ) {
+function cls_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
@@ -26,17 +26,17 @@ function gutenberg_starter_theme_body_classes( $classes ) {
 	}
 	return $classes;
 }
-add_filter( 'body_class', 'gutenberg_starter_theme_body_classes' );
+add_filter( 'body_class', 'cls_body_classes' );
 
 /**
  * Add a pingback url auto-discovery header for singularly identifiable articles.
  */
-function gutenberg_starter_theme_pingback_header() {
+function cls_pingback_header() {
 	if ( is_singular() && pings_open() ) {
 		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
 	}
 }
-add_action( 'wp_head', 'gutenberg_starter_theme_pingback_header' );
+add_action( 'wp_head', 'cls_pingback_header' );
 
 /**
  * Register post type templates
